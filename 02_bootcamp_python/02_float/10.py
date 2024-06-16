6654f5fad0c503443fff9fc1996553670b17b8e6
Fellipe Guedes <fellipe.guedes@outlook.com>
2024-06-16 11:13:08 -0300

    refatorando o ex. 10

--- 02_bootcamp_python/02_float/10.py
+++ 02_bootcamp_python/02_float/10.py
@@ -1,6 +1,6 @@
 #Escreva um programa que calcule a área de um círculo, recebendo o raio como entrada.
 import math
-n1 = float(input("Digite o raio do circulo para cálculo da área"))
+n1 = float(input("Digite o raio do circulo para cálculo da área: "))
 pi = math.pi
-area = pi * n1 * n1
-print(area)
+area = pi * n1 ** 2
+print(f"{area:.2f}")
